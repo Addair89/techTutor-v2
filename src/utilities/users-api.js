2b10e64ed304058d21260ba9f713abde9865bdcb
Jared Addair <addairjared@gmail.com>
2024-05-07 13:44:48 -0700

    changes

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,5 +1,5 @@
 import sendRequest from "./send-request";
-const BASE_URL = "/api/users";
+const BASE_URL = "https://tech-tutor-v2.vercel.app/api/users";
 
 export async function signUp(userData) {
   return sendRequest(`${BASE_URL}/`, "POST", userData);
